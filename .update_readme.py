060679928a73b52ffa68c7102b7770acdbacebc2
typoverflow <typoverflow@outlook.com>
2021-04-15 23:31:55 +0800

    Add README

---
+++ .update_readme.py
@@ -0,0 +1,18 @@
+import re
+import sys
+import pathlib
+
+r = re.compile(
+    r"<!\-\- BEGIN:TAG \-\->.*<!\-\- END:TAG \-\->", 
+    re.DOTALL
+)
+
+chunk = "<!-- BEGIN:TAG -->\n{}\n<!-- END:TAG -->".format("---\n\n最新的提交者： :bot:\n\n---")
+
+root = pathlib.Path(__file__).parent.resolve()
+readme = root/"README.md"
+
+rewritten = readme.open().read()
+rewritten = r.sub(chunk, rewritten)
+
+readme.open("w").write(rewritten)
